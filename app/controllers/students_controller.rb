f22da98778ee092e84f45363482f183f9cbf7a74
Joey Cho <cjy12182000@naver.com>
2019-02-14 11:00:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,8 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
   end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
