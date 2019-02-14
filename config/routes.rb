8bdb58ed4dd95a09ea00894a6876a2b31b8f2458
Joey Cho <cjy12182000@naver.com>
2019-02-14 11:17:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students
   resources :school_classes
   resources :students
   # The priority is based upon order of creation: first created -> highest priority.
